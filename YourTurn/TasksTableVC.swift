b32078f75c349592fb88ccc6283ec9771a741dbe
Vamsi Punna <vmzi@MacBook-Pro.local>
2017-06-24 19:34:22 -0400

    Not reloading the row on tasks table vc, if the same user is selected in taskview controller

--- YourTurn/TasksTableVC.swift
+++ YourTurn/TasksTableVC.swift
@@ -81,6 +81,14 @@ class TasksTableVC: UITableViewController {
     
     override func viewDidAppear(_ animated: Bool) {
         if(selectedTaskCellRow >= 0 && selectedTaskCellRow < tasks.count){
+            
+            // do not animate if the same user is selected
+            if let name = tasks[selectedTaskCellRow]["NextTurnUserName"]{
+                if selectedTaskNextUserName == name as! String{
+                    return
+                }
+            }
+            
             tasks[selectedTaskCellRow]["NextTurnUserName"] = selectedTaskNextUserName
             self.tableView.reloadRows(at: [IndexPath(row: selectedTaskCellRow, section: 0)], with: UITableViewRowAnimation.top)
         }
